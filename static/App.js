f9ecbf94c4b4fdb29f4f1d9ff4c2272604688fab
lsshawn <coffee.or.tea88@gmail.com>
2017-07-16 05:13:28 +0000

    Update

--- static/App.js
+++ static/App.js
@@ -216,6 +216,16 @@ var IssueAdd = function (_React$Component3) {
   return IssueAdd;
 }(React.Component);
 
+var issues = [{
+  id: 1, status: 'Open', owner: 'Ravan',
+  created: new Date('2016-08-15'), effort: 5,
+  completionDate: undefined, title: 'Error in console when clicking Add'
+}, {
+  id: 2, status: 'Assigned', owner: 'Eddie',
+  created: new Date('2016-08-16'), effort: 14,
+  completionDate: new Date('2016-08-30'), title: 'Missing bottom border on panel'
+}];
+
 var IssueList = function (_React$Component4) {
   _inherits(IssueList, _React$Component4);
 
